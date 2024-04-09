bb27e75ea1ea2c7e6485c5495a5e7ae32676b6cd
roberto-palanga <r.palanga94@gmail.com>
2024-04-09 18:38:39 +0200

    inserimento immagini

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,20 @@
     <div class="container-fluid">
 
         <div class="container text-center py-4">
-            <h2 class="my-4">Scegli dalla nostra lista</h2>
-
+            <h2 class="my-4 display-3 fw-bolder text-success-emphasis ">Scegli dalla nostra lista</h2>
+            
             <div class="mycard-container row mx-auto gap-5 justify-content-center  ">
                 
                 @foreach ($movies as $movie)
-                <div class="card col-5 ">
-                    <img src="..." class="card-img-top" alt="...">
+                <div class="card col-5 p-0  shadow  border-0 ">
+                    <img src="{{$movie->poster}}" class="card-img-top" alt="...">
                     <div class="card-body">
-                      <h5 class="card-title">{{$movie->title}}</h5>
-                      <p class="card-text">{{$movie->original_title}}</p>
+                      <h3 class="card-title fw-bolder ">{{$movie->title}}</h3>
+                      <p class="card-text fw-bold ">{{$movie->original_title}}</p>
+                      <p class="card-text fw-light">{{$movie->nationality}}</p>
+                      <p class="card-text fw-lighter ">{{$movie->vote}}</p>
+
+
                       
                     </div>
                   </div>
