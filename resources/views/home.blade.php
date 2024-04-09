b257aea4a0327f1909fe5d1e0d6dc26e3398b468
roberto-palanga <r.palanga94@gmail.com>
2024-04-09 18:18:38 +0200

    inserimento elementi database manca stilizzazione

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,26 @@
 
     <div class="container-fluid">
 
-        <div class="container ">
+        <div class="container text-center py-4">
+            <h2 class="my-4">Scegli dalla nostra lista</h2>
+
+            <div class="mycard-container row mx-auto gap-5 justify-content-center  ">
+                
+                @foreach ($movies as $movie)
+                <div class="card col-5 ">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">{{$movie->title}}</h5>
+                      <p class="card-text">{{$movie->original_title}}</p>
+                      
+                    </div>
+                  </div>
+                    
+                @endforeach
+
+            </div>
             
-            <h1>Ciao a tutti</h1>
-            
+
         </div>
     </div>
 
