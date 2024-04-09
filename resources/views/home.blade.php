353f17d711c4dd32744b09fa8ae367a029599c89
roberto-palanga <r.palanga94@gmail.com>
2024-04-09 15:51:47 +0200

    inizio esercizio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,14 @@
 @extends('layout/app')
 
 @section('content')
-    <div class="container">
-        
-        <h1>Ciao a tutti</h1>
+
+    <div class="container-fluid">
+
+        <div class="container ">
+            
+            <h1>Ciao a tutti</h1>
+            
+        </div>
     </div>
 
 @endsection
