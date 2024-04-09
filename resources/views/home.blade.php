4bd6459b815245d11ee644734dfcd0f8bcbf9529
roberto-palanga <r.palanga94@gmail.com>
2024-04-09 18:56:55 +0200

    esercizio completato con bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,18 @@
     <div class="container-fluid">
 
         <div class="container text-center py-4">
-            <h2 class="my-4 display-3 fw-bolder text-success-emphasis ">Scegli dalla nostra lista</h2>
+            {{-- <h2 class="my-4 display-3 fw-bolder text-success-emphasis ">Scegli dalla nostra lista</h2>
+             --}}
+             <div class="lightning py-4 w-75 mx-auto">
+                <div class="noisy">
+                  <span> for you</span>
+                  the
+                </div>
+                <div class="noisy">
+                  movie
+                  <span>with Love by Roberto</span>
+                </div>
+              </div>
             
             <div class="mycard-container row mx-auto gap-5 justify-content-center  ">
                 
